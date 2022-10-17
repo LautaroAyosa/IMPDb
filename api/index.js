a39191faac45577086a109a3b24af8f6005e9f87
lautaroayosa <lautaroayosa@gmail.com>
2022-10-17 19:50:40 -0300

    added comments

--- api/index.js
+++ api/index.js
@@ -8,16 +8,18 @@ const server = http.createServer(app)
 
 // Verify connection to the DB and listen app
 const main = async () => {
-
   try {
+    // try the connection to sequelize 
     await sequelize.authenticate()
     logger.info('Connected to the DB successfully')
 
+    // listen app at port #
     const PORT = config.PORT || 3003
     server.listen(PORT, () => {
       logger.info(`Server running on port ${PORT}`)
     })
   } catch (err) {
+    // log error if the connection with sequelize failed
     logger.info('Unable to connect to the database:', err)
   }
 }
