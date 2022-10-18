118e27c743891b0d4dce35f82480f0f0f9e9bc1f
lautaroayosa <lautaroayosa@gmail.com>
2022-10-18 20:51:46 -0300

    Filter

---
+++ client/src/components/Filter/Filter.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { setFilter } from '../../redux/reducers/filterReducer';
+
+const Filter = (props) => {
+    const filter = useSelector( state => state.filter)
+    const dispatch = useDispatch()
+
+    const handleFilterChange = (e) => {
+        dispatch(setFilter(e.target.value))
+    }
+
+    return (
+        <div className='filterContainer'><input className='filter' onChange={handleFilterChange} value={filter} name='filter' placeholder={props.placeholder ? props.placeholder : 'Search blogs and more' } /></div>
+    )
+}
+
+export default Filter;
