89164acd2cd7ba53bd0d0b2378ecf02ded5115d4
lautaroayosa <lautaroayosa@gmail.com>
2022-11-13 21:32:01 -0300

    config changes for railway

--- api/models/index.js
+++ api/models/index.js
@@ -6,7 +6,7 @@ const sequelize = new Sequelize (
     config.DB_NAME, 
     config.DB_USER, 
     config.DB_PASSWORD, {
-    host: 'localhost',
+    host: config.DB_HOST,
     dialect: 'postgres'
 })
 
